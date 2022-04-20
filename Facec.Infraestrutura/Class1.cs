a790c146bd1c9cf93983f08b383dca57c299cfa5
Humberto Petita <humbertopetita@gmail.com>
2022-04-19 22:31:55 -0300

    inicial

---
+++ Facec.Infraestrutura/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Facec.Infraestrutura
+{
+    public class Class1
+    {
+    }
+}
---
+++ Facec.WebApi/Controllers/ClienteController.cs
@@ -0,0 +1,29 @@
+﻿using Facec.Dominio.nsEntidades;
+using Facec.Dominio.nsInterfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Facec.WebApi.Controllers
+{
+    [ApiController]
+    [Route("clientes")]
+    public class ClienteController
+    {
+        private readonly IClienteServico _servico;
+
+        public ClienteController(IClienteServico servico)
+        {
+            _servico = servico;
+        }
+
+        [HttpDelete]
+        public void Excluir(Cliente cliente)
+            => _servico.Excluir(cliente);
+
+        [HttpPost]
+        public void Gravar(Cliente cliente)
+            => _servico.Gravar(cliente);
+
+        public IEnumerable<Cliente> Obter()
+            => _servico.Obter();
+    }
+}
